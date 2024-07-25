9d203b3ef43f00176a88c3822bc3bba14f3636b4
Artur Bai <artur.yb1@gmail.com>
2024-07-24 21:12:21 -0400

    add x icon to close button

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -11,7 +11,19 @@ const TaskDetails = ({ task, onClose }) => {
             <h2>{task.taskName}</h2>
             <StatusChip status={task.status} />
           </div>
-          <button onClick={onClose}>Close</button>
+          <button className="close-button" onClick={onClose}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path
+                fill="currentColor"
+                d="M18.3 5.71a1 1 0 0 0-1.42 0L12 10.59 7.12 5.71A1 1 0 0 0 5.71 7.12L10.59 12l-4.88 4.88a1 1 0 1 0 1.41 1.42L12 13.41l4.88 4.88a1 1 0 0 0 1.42-1.41L13.41 12l4.88-4.88a1 1 0 0 0 0-1.41z"
+              />
+            </svg>
+          </button>
         </div>
         <div className="task-details-content">
           <div className="task-info">
